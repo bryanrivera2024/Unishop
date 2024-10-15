ce12b03536cdb944386b9925fc781f0370d9f49e
bryanrivera2024 <bryan.rivera2024@hilasal.superate.org.sv>
2024-10-14 21:11:08 -0600

    Se agrego el navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+
+<nav className="navbar bg-body-tertiary" style={{ height: '100vh', backgroundImage: "url('https://poemanalysis.com/wp-content/uploads/2022/10/Going-to-See-King-Lear-by-Jackie-Kay-Visual-Representation.jpg')" }}>
+</nav>
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      {/* Menú de navegación */}
+      <header>
+        <nav className="navbar">
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#profile">Profile</a></li>
+            <li><a href="#categories">Categories</a></li>
+          </ul>
+        </nav>
+        <div className="linea"></div>
       </header>
+
+
+
+
+      {/* Banner y contenido */}
+      <section className="banner">
+        <h1>Our New Collection</h1>
+        <input type="text" placeholder="Search new collection..." className="search-bar" />
+      </section>
+
+      <section className="main-content">
+        <div className="buttons-container">
+          <button>New Collection</button>
+          <button>Special Promo</button>
+          <button>Casual Bag</button>
+          <button>Party Bag</button>
+        </div>
+
+        
+
+        <div className="images-container">
+          <div className="item">
+            <img src="/images/producto1.jpg" alt="Producto 1" />
+            <p>Shoes</p>
+          </div>
+          <div className="item">
+            <img src="" alt="Producto 2" />
+            <p>Clothes</p>
+          </div>
+          <div className="item">
+            <img src="/images/producto3.jpg" alt="Producto 3" />
+            <p>Appliances</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
 
 export default App;
+
